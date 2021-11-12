2edf00e00341b228326d863199eba3727d9d42cc
Kyle Mesdag <mesd0002@algonquinlive.com>
2021-11-12 11:24:43 -0500

    Updated assignment 2

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-const $imageGallery = document.querySelectorAll(".image-gallery")
+const $imageGallery = document.querySelector ("image-1")
+
+imageGallery.innerHTML ='<img width="100" height="100" src="images/image1.jpg">';
 
